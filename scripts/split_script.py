22568538576c32075bf5b00424fb7201fa125f20
rujinwang <rujinwang1124@gmail.com>
2019-03-04 15:43:37 -0500

    add demultiplexing py file

--- scripts/split_script.py
+++ scripts/split_script.py
@@ -9,7 +9,6 @@ with open("/pine/scr/r/u/rujin/10XGenomics/breast_tissue_A_2k/output/breast_tiss
         # get the barcode list from the 1st column
         bclist=r[0]
         data.append(bclist)
-# print(data)
 # total count of reads
 tcount=0
 bcount=0
@@ -18,54 +17,25 @@ for line in fileinput.input():
     line=line.strip()
     tags = line.split()[11:]
     tags_sort = sorted(tags)
-    # print(tags_sort)
-    # count of reads containing identified barcodes
     for tag in tags_sort:
 	tag_split=tag.split(':')
-	# print(tag_split)
         if 'CB' in tag_split:
 	    barcode=tag_split[-1]
-	    # print(barcode)
 	    if barcode in data:
             	bcount+=1
             	print(str(tcount)+" "+str(bcount))
-		# directory = barcode[:5]
 	    	directory = "../align/"+barcode[:-2]
-	    	# print(directory)
             	outfile = directory+"/"+barcode+".sam"
             	try:
                     f=open(outfile,"a+")
-                    # print(line, file=f)
 		    print >> f,line
-		    # print line
                     f.close()
             	except:
                     os.mkdir(directory)
                     f=open(outfile,"a+")
-                    # print(line, file=f)
 		    print >> f,line
-		    # print line
                     f.close()
 	    	break
 pct=float(bcount)/float(tcount)
 print(pct)
-	# elif 'CR' in tag_split:
-            # barcode=tag_split[-1]+"-1"
-	    # print(barcode)
-            # directory = barcode[:5]
-	    # directory = "../align/"+barcode[:-2]
-	    # print(directory)
-            # outfile = directory+"/"+barcode+".sam"
-	    # try:
-        	# f=open(outfile,"a+")
-                # print(line, file=f)
-		# print >> f,line
-		# print line
-                # f.close()
-            # except:
-                # os.mkdir(directory)
-                # f=open(outfile,"a+")
-                # print(line, file=f)
-		# print >> f,line
-		# print line
-                # f.close()
+
