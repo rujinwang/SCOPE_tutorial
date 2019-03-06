4c07eaabba124a078e83ff1d36ccb4e900cc3dc0
rujinwang <rujinwang1124@gmail.com>
2019-03-06 10:06:01 -0500

    update SCOPE normalization

--- package/R/normalize_scope.R
+++ package/R/normalize_scope.R
@@ -6,7 +6,7 @@
 #'  content bias using an expectation-maximization algorithm, which accounts for the different
 #'  copy number states.
 #'
-#' @param Y read depth matrix after quality control
+#' @param Y_qc read depth matrix after quality control
 #' @param gc_qc vector of GC content for each bin after quality control
 #' @param K Number of latent Poisson factors
 #' @param norm_index indices of normal/diploid cells
@@ -35,13 +35,13 @@
 #' @author Rujin Wang \email{rujin@email.unc.edu}
 #' @import stats
 #' @export
-normalize_scope = function (Y, gc_qc, K, norm_index, N, T, alpha0, beta0, minCountQC = 20) {
+normalize_scope = function (Y_qc, gc_qc, K, norm_index, N, T, alpha0, beta0, minCountQC = 20) {
   if (max(K) > length(norm_index))
     stop("Number of latent Poisson factors K cannot exceed the number of
          normal samples!")
   Ntotal <- N
-  N <- round(N/median(N)*median(colSums(Y)))
-  Nmat <- matrix(nrow = nrow(Y), ncol = ncol(Y), data = N, byrow = TRUE)
+  N <- round(N/median(N)*median(colSums(Y_qc)))
+  Nmat <- matrix(nrow = nrow(Y_qc), ncol = ncol(Y_qc), data = N, byrow = TRUE)
 
   Yhat = vector('list', length(K))
   fGC.hat <- vector('list', length(K))
@@ -55,9 +55,9 @@ normalize_scope = function (Y, gc_qc, K, norm_index, N, T, alpha0, beta0, minCou
 
   # Initialization
   message("Initialization ...")
-  gcfit.temp=Y/Nmat/beta0
-  offset=Nmat*matrix(nrow = nrow(Y), ncol = ncol(Y), data = beta0, byrow = FALSE)
-  fhat.temp=getfGC(gcfit.temp = gcfit.temp, gctemp = gc_qc, Y = Y, norm_index = norm_index, offset = offset, T = T, alpha = alpha0, minCountQC = minCountQC)
+  gcfit.temp=Y_qc/Nmat/beta0
+  offset=Nmat*matrix(nrow = nrow(Y_qc), ncol = ncol(Y_qc), data = beta0, byrow = FALSE)
+  fhat.temp=getfGC(gcfit.temp = gcfit.temp, gctemp = gc_qc, Y = Y_qc, norm_index = norm_index, offset = offset, T = T, alpha = alpha0, minCountQC = minCountQC)
   fhat0=fhat.temp$fGC.hat
   alpha0=fhat.temp$alpha
 
@@ -70,12 +70,12 @@ normalize_scope = function (Y, gc_qc, K, norm_index, N, T, alpha0, beta0, minCou
     BHTHRESH = 1e-04
     HHTHRESH = 1e-05
     iter = 1
-    fhat = matrix(nrow = nrow(Y), ncol = ncol(Y), data = 0)
+    fhat = matrix(nrow = nrow(Y_qc), ncol = ncol(Y_qc), data = 0)
     betahat = beta0
-    betahatmat = matrix(nrow = nrow(Y), ncol = ncol(Y),
+    betahatmat = matrix(nrow = nrow(Y_qc), ncol = ncol(Y_qc),
                         data = betahat, byrow = FALSE)
-    ghat = matrix(0, nrow = nrow(Y), ncol = k)
-    hhat = matrix(0, nrow = ncol(Y), ncol = k)
+    ghat = matrix(0, nrow = nrow(Y_qc), ncol = k)
+    hhat = matrix(0, nrow = ncol(Y_qc), ncol = k)
     bhdiff = rep(Inf, maxiter)
     fhdiff = rep(Inf, maxiter)
 
@@ -88,15 +88,15 @@ normalize_scope = function (Y, gc_qc, K, norm_index, N, T, alpha0, beta0, minCou
     while (iter <= maxiter) {
       if(iter==1){fhatnew=fhat0; alpha=alpha0}
       if(iter>1){
-        gcfit.temp=Y/Nmat/betahat/exp(ghat %*% t(hhat))
-        offset=Nmat*matrix(nrow = nrow(Y), ncol = ncol(Y),
+        gcfit.temp=Y_qc/Nmat/betahat/exp(ghat %*% t(hhat))
+        offset=Nmat*matrix(nrow = nrow(Y_qc), ncol = ncol(Y_qc),
                            data = betahat, byrow = FALSE)*exp(ghat %*% t(hhat))
-        fhat.temp=getfGC(gcfit.temp=gcfit.temp, gctemp=gc_qc, Y=Y, norm_index=norm_index, offset=offset, T = T, alpha=alpha0, minCountQC = minCountQC)
+        fhat.temp=getfGC(gcfit.temp=gcfit.temp, gctemp=gc_qc, Y=Y_qc, norm_index=norm_index, offset=offset, T = T, alpha=alpha0, minCountQC = minCountQC)
         fhatnew=fhat.temp$fGC.hat
         alpha=fhat.temp$alpha
       }
       fhatnew[fhatnew<quantile(fhatnew, 0.005)]=quantile(fhatnew, 0.005)
-      betahatnew = apply((Y/(fhatnew * Nmat * exp(ghat %*% t(hhat))))[,norm_index], 1, median)
+      betahatnew = apply((Y_qc/(fhatnew * Nmat * exp(ghat %*% t(hhat))))[,norm_index], 1, median)
       betahatnew[betahatnew <= 0] = min(betahatnew[betahatnew > 0])
       bhdiff[iter] = sum((betahatnew - betahat)^2)/length(betahat)
       fhdiff[iter] = sum((fhatnew - fhat)^2)/length(fhat)
@@ -106,21 +106,21 @@ normalize_scope = function (Y, gc_qc, K, norm_index, N, T, alpha0, beta0, minCou
               signif(bhdiff[iter], 3), "\t", "f(GC) diff =", signif(fhdiff[iter], 3))
       fhat = fhatnew
       betahat = betahatnew
-      betahatmat = matrix(nrow = nrow(Y), ncol = ncol(Y),
+      betahatmat = matrix(nrow = nrow(Y_qc), ncol = ncol(Y_qc),
                           data = betahat, byrow = FALSE)
       L = log(Nmat * fhat * betahatmat * alpha / 2)
-      logmat = log(pmax(Y, 1)) - L
-      logmat = logmat - matrix(nrow = nrow(Y), ncol = ncol(Y), data = apply(logmat, 1, mean), byrow = FALSE)
+      logmat = log(pmax(Y_qc, 1)) - L
+      logmat = logmat - matrix(nrow = nrow(Y_qc), ncol = ncol(Y_qc), data = apply(logmat, 1, mean), byrow = FALSE)
       hhat = svd(logmat, nu = k, nv = k)$v
       hhatnew = hhat
       hiter = 1
       hhdiff = rep(Inf, maxhiter)
       while (hiter <= maxhiter) {
-        for (s in 1:nrow(Y)) {
-          temp=try(glm(formula = Y[s,norm_index] ~ hhat[norm_index,] -
+        for (s in 1:nrow(Y_qc)) {
+          temp=try(glm(formula = Y_qc[s,norm_index] ~ hhat[norm_index,] -
                          1, offset = L[s,norm_index], family = poisson)$coefficients,silent=TRUE)
           if(is.character(temp)){
-            temp=lm(log(pmax(Y[s,norm_index],1)) ~ hhat[norm_index,] -
+            temp=lm(log(pmax(Y_qc[s,norm_index],1)) ~ hhat[norm_index,] -
                       1, offset = log(L[s,norm_index]))$coefficients
           }
           ghat[s, ] = temp
@@ -137,8 +137,8 @@ normalize_scope = function (Y, gc_qc, K, norm_index, N, T, alpha0, beta0, minCou
             z[z<quantile(z,0.005)] = max(quantile(z,0.005),-30)
             z})
         }
-        for (t in 1:ncol(Y)) {
-          hhatnew[t, ] = glm(formula = Y[, t] ~ ghat -
+        for (t in 1:ncol(Y_qc)) {
+          hhatnew[t, ] = glm(formula = Y_qc[, t] ~ ghat -
                                1, offset = L[, t], family = poisson)$coefficients
         }
         gh = ghat %*% t(hhatnew)
@@ -171,7 +171,7 @@ normalize_scope = function (Y, gc_qc, K, norm_index, N, T, alpha0, beta0, minCou
     betahat = betahatlist[[optIter]]
     ghat = ghatlist[[optIter]]
     hhat = hhatlist[[optIter]]
-    betahatmat = matrix(nrow = nrow(Y), ncol = ncol(Y),
+    betahatmat = matrix(nrow = nrow(Y_qc), ncol = ncol(Y_qc),
                         data = betahat, byrow = FALSE)
     Yhat[[ki]] = pmax(round(fhat * Nmat * betahatmat * exp(ghat %*% t(hhat)),0),1)
     alpha.hat[[ki]] <- alpha
@@ -180,11 +180,11 @@ normalize_scope = function (Y, gc_qc, K, norm_index, N, T, alpha0, beta0, minCou
     h.hat[[ki]] <- signif(hhat, 3)
     g.hat[[ki]] <- signif(ghat, 3)
     Yhat.temp=Yhat[[ki]]*alpha/2
-    AIC[ki] = 2 * sum(Y * log(pmax(Yhat.temp,1)) - Yhat.temp) -
+    AIC[ki] = 2 * sum(Y_qc * log(pmax(Yhat.temp,1)) - Yhat.temp) -
       2 * (length(ghat) + length(hhat))
-    BIC[ki] = 2 * sum(Y * log(pmax(Yhat.temp,1)) - Yhat.temp) -
-      (length(ghat) + length(hhat)) * log(length(Y))
-    RSS[ki] = sum((Y - Yhat.temp)^2/length(Y))
+    BIC[ki] = 2 * sum(Y_qc * log(pmax(Yhat.temp,1)) - Yhat.temp) -
+      (length(ghat) + length(hhat)) * log(length(Y_qc))
+    RSS[ki] = sum((Y_qc - Yhat.temp)^2/length(Y_qc))
     message("AIC", k, " = ", round(AIC[ki], 3))
     message("BIC", k, " = ", round(BIC[ki], 3))
     message("RSS", k, " = ", round(RSS[ki], 3), "\n")
